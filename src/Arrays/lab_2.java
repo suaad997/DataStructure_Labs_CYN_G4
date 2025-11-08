f796f1c0ba20d34335cae0ca3675f72522f72e0b
suada <suadaljuaid1@gmail.com>
2025-11-08 22:16:18 +0300

    lab2

--- src/Arrays/lab_2.java
+++ src/Arrays/lab_2.java
@@ -7,7 +7,7 @@ public class lab_2 {
     public static void main(String[] args) {
         //3   Scanner scanner=new Scanner(System.in);
         Array arr =new Array();
-        //int []numbers={1,2,3,5};
+        //int []numbers={1,2,3};
         int []numbers=new int[3];
         arr.input(numbers);
         arr.traversal(numbers);
